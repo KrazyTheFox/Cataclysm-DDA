858b7ff82e2de8b137b5d5afe99c41e538c65220
Mitchell Matthews <mmatthews@suprtek.com>
2016-08-23 15:01:52 -0400

    Add tour guide profession

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-make CROSS="~/src/mxe/usr/bin/x86_64-w64-mingw32.static-" --jobs TILES=1 LUA=1 RELEASE=1 LOCALIZE=1 USE_HOME_DIR=0 CCACHE=1
+make CROSS="~/src/mxe/usr/bin/x86_64-w64-mingw32.static-" --jobs=8 TILES=1 LUA=1 RELEASE=1 LOCALIZE=1 USE_HOME_DIR=0 CCACHE=1
